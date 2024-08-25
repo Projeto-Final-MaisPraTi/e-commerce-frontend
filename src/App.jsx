557f123c5bcc419c97ea78069214420e77e3d843
JoseCarlosVSJ <josecarlosjunior027@gmail.com>
2024-08-25 12:30:37 -0300

    Criacao do footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./styles.jsx";
+import Footer from "./components/ComponentFooter.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
       <h1>Hello World</h1>
+      <Footer />
     </>
   );
 }
