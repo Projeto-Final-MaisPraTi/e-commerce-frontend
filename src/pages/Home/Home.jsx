edf3949deba37227bf69cb2670dfd754b0c6bef7
MarlonMuller <72674862+MarlonMuller@users.noreply.github.com>
2024-09-13 20:48:47 -0300

    First version

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/ComponentFooter";
 import FeaturesSection from "../../components/FeaturesSection/FeaturesSection";
+import FlashSales from "../../components/FlashSales/FlashSales";
 function Home() {
   return (
     <>
       <Header />
+      <FlashSales />
       <FeaturesSection />
       <Footer />
     </>
