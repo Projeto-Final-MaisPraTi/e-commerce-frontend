59c6eaf6d167c98590ca10bf33d6e2fb40ff1249
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-09-29 12:51:30 -0300

    Criada função para alterar imagens ao passar o mouse. Criadas props para envio de dados do produto. Removido size, botões de quantidade e ícone de coração. Criada função para inserir dados do produto, mas ainda não redireciona. Feitos outros ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './styles.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header.jsx';
-import Buttons from './components/buttons/Buttons.jsx';
+// import Buttons from './components/buttons/Buttons.jsx';
 import Footer from './components/Footer/ComponentFooter.jsx';
 import ProductDetails from './components/ProductDetails/ProductDetails.jsx';
 
@@ -13,7 +13,11 @@ function App() {
       <Buttons type={1} text={'add to cart type 1'} />
       <Buttons type={2} text={'buy now type 2'} />
       <Buttons type={3} text={'add to wishlist type 3'} /> */}
-      <ProductDetails />
+      <ProductDetails
+        productName={'Havic HV G-92 Gamepad'}
+        price={192.0}
+        quantity={2}
+      />
       <Footer />
     </>
   );
