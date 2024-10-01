11d90516dd815303c332579eb739cd6e8fb566ef
Gabriel <gabriel_bertollo2002@hotmail.com>
2024-10-01 15:04:00 -0300

    ajustes do carrossel com as categorias

--- src/components/categoryWithCarousel/Category.jsx
+++ src/components/categoryWithCarousel/Category.jsx
@@ -1,44 +1,38 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-const LinkList = styled.div`
-  border-right: solid 1px;
-  border-color: gray;
-  margin-bottom: 0;
-  padding-top: 20px;
-  padding-right: 10px;
-  align-self: baseline;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: end;
-  justify-content: center;
-  margin-right: 10px;
+const LinkList=styled.div`
+margin-bottom: 0;
+padding-top: 40px;
+padding-left: 0;
+align-self: baseline;
+display: flex;
+flex-direction: column;
+align-items: baseline;
+justify-content: center;
 
-  a {
-    color: #000;
-    text-decoration: none;
-    margin-top: 10px;
-    &:hover {
-      border-bottom: solid 1px;
-    }
-  }
-
+a{
+color: #000;
+text-decoration: none;
+margin-top: 10px;
+&:hover {
+    border-bottom: solid 1px;
+  }}
   @media (max-width: 767px) {
-    border-right: none;
     align-items: center;
   }
+  
 `;
 
 function Category() {
-  return (
-    <LinkList className="col-12 col-md-2">
-      <a href="">Phones</a>
-      <a href="">Computers</a>
-      <a href="">SmartWatch</a>
-      <a href="">Camera</a>
-      <a href="">HeadPhones</a>
-    </LinkList>
-  );
+    return(
+        <LinkList className='col-12 col-md-2'>
+        <a href="">Phones</a>
+        <a href="">Computers</a>
+        <a href="">SmartWatch</a>
+        <a href="">Camera</a>
+        <a href="">HeadPhones</a>
+        </LinkList>
+        )
 }
 
 export default Category;
