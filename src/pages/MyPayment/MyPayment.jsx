13a78d932876fbe2c6640b584b23be025349ea34
luiznslobato <contato.luiznonato@gmail.com>
2024-10-13 14:47:16 -0300

    feat: MyPayment.jsx

---
+++ src/pages/MyPayment/MyPayment.jsx
@@ -0,0 +1,17 @@
+import Header from "../../components/Header/Header";
+import Payment from "../../components/Payment/Payment";
+import Footer from "../../components/Footer/ComponentFooter";
+
+function MyPayment() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Payment />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default MyPayment;
