4020bb7405544b14c7aa910d2a6d8f486aedc8ac
gabrielwillianfb <gabriel.beck03@gmail.com>
2024-10-16 16:53:14 -0300

    estrutura inicial do NewArrival

--- src/components/NewArrival/newarrival.jsx
+++ src/components/NewArrival/newarrival.jsx
@@ -12,37 +12,37 @@ const NewArrival = () => {
       <h2>New Arrival</h2>
 
       {/* Grid de Produtos */}
-      <div className="product-grid">
-        <div className="product-card large">
-          <img src="https://via.placeholder.com/400x300" alt="PlayStation 5" />
-          <div className="newarrival-product-info">
+      <div className="grid-container">
+        <div className="box box1">
+          <img src="https://placehold.co/570x600" alt="PlayStation 5" />
+          <div className="product-info">
             <h2>PlayStation 5</h2>
             <p>Black and White version of the PS5 coming out on sale.</p>
-            <button className="newarrival-button">Shop Now</button>
+            <button className="shop-button">Shop Now</button>
           </div>
         </div>
-        <div className="product-card medium">
-          <img src="https://via.placeholder.com/400x300" alt="Women's Collection" />
-          <div className="newarrival-product-info">
+        <div className="box box2">
+          <img src="https://placehold.co/570x184" alt="Women's Collection" />
+          <div className="product-info">
             <h2>Women&apos;s Collections</h2>
             <p>Featured women collections that give you another vibe.</p>
-            <button className="newarrival-button">Shop Now</button>
+            <button className="shop-button">Shop Now</button>
           </div>
         </div>
-        <div className="product-card small">
-          <img src="https://via.placeholder.com/400x300" alt="Speakers" />
-          <div className="newarrival-product-info">
+        <div className="box box3">
+          <img src="https://placehold.co/270x184" alt="Speakers" />
+          <div className="product-info">
             <h2>Speakers</h2>
             <p>Amazon wireless speakers.</p>
-            <button className="newarrival-button">Shop Now</button>
+            <button className="shop-button">Shop Now</button>
           </div>
         </div>
-        <div className="product-card small">
-          <img src="https://via.placeholder.com/400x300" alt="Perfume" />
-          <div className="newarrival-product-info">
+        <div className="box box4">
+          <img src="https://placehold.co/270x184" alt="Gucci Perfume" />
+          <div className="product-info">
             <h2>Gucci Perfume</h2>
             <p>Gucci Intense Oud.</p>
-            <button className="newarrival-button">Shop Now</button>
+            <button className="shop-button">Shop Now</button>
           </div>
         </div>
       </div>
