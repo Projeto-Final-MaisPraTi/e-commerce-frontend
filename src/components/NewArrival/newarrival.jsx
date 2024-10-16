8efd8768f94da756becb53befda3b7db7c479f1a
EnzoJ11 <janssgamer0@gmail.com>
2024-10-16 17:35:52 -0300

    New Arrival finalizado

--- src/components/NewArrival/newarrival.jsx
+++ src/components/NewArrival/newarrival.jsx
@@ -1,21 +1,57 @@
 import "./NewArrival.css";
+import Ps5 from "../../assets/PS5slim.png"
+import Mulher from "../../assets/mulher.png"
+import Speakers from "../../assets/speakers.png"
+import Perfume from "../../assets/Perfume.png"
+import { Link } from "react-router-dom";
 
 const NewArrival = () => {
   return (
-    <section className="new-arrival">
-      <div className="featured">
-        <span className="featured-indicator"></span>
-        <h2 className="featured-title">Featured</h2>
-      </div>
-      <h1 className="main-title">New Arrival</h1>
+    <div className="new-arrival-section">
+      {/* Destaque */}
 
-      <div className="products-grid">
-        <div className="product large" ></div>
-        <div className="product medium" ></div>
-        <div className="product small" ></div>
-        <div className="product smaller" ></div>
+      <div className="featured">
+        <span className="featured-tag"></span>
+        <p>Featured</p>
       </div>
-    </section>
+      <h2>New Arrival</h2>
+
+      {/* Grid de Produtos */}
+      <div className="grid-container">
+        <div className="box box1">
+          <img src={Ps5} alt="PlayStation 5" />
+          <div className="product-info">
+            <h2>PlayStation 5</h2>
+            <p>Black and White version of the PS5 coming out on sale.</p>
+            <Link to="/" className="shop-button">Shop Now</Link>
+          </div>
+        </div>
+        <div className="box box2">
+          <img src={Mulher} alt="Women's Collection" />
+          <div className="product-info">
+            <h2>Women&apos;s Collections</h2>
+            <p>Featured women collections that give you another vibe.</p>
+            <Link to="/" className="shop-button">Shop Now</Link>
+          </div>
+        </div>
+        <div className="box box3">
+          <img src={Speakers} alt="Speakers" />
+          <div className="product-info">
+            <h2>Speakers</h2>
+            <p>Amazon wireless speakers.</p>
+            <Link to="/" className="shop-button">Shop Now</Link>
+          </div>
+        </div>
+        <div className="box box4">
+          <img src={Perfume} alt="Gucci Perfume" />
+          <div className="product-info">
+            <h2>Gucci Perfume</h2>
+            <p>Gucci Intense Oud.</p>
+            <Link to="/" className="shop-button">Shop Now</Link>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
