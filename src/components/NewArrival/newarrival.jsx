2da6bb0b0fb7497b4cac3b3fda1165991b0ade35
gabrielwillianfb <gabriel.beck03@gmail.com>
2024-10-30 13:07:08 -0300

    Fix: ProductsData, NewArrival, Smartwhatches images

--- src/components/NewArrival/newarrival.jsx
+++ src/components/NewArrival/newarrival.jsx
@@ -1,6 +1,6 @@
 import styles from "./NewArrival.module.css";
 import G933 from "../../assets/headphones/Logitech_G933_black_1.png";
-import Ultra from "../../assets/smartwatches/Apple_Watch_Series_Ultra_orange_1.png";
+import Ultra from "../../assets/smartwatches/Apple_Watch_Series_Ultra_black_1.png";
 import Zenbook from "../../assets/computers/Asus_Zenbook_S14_black_1.png";
 import Iphone15 from "../../assets/phones/Apple_Iphone_15_pro_grey_1.png";
 import { Link } from "react-router-dom";
@@ -21,7 +21,7 @@ const NewArrival = () => {
           <img src={G933} alt="Logitech G933" />
           <div className={styles.productInfo}>
             <h2>Logitech G933</h2>
-            <p>One of the best gaming headsets on the market.</p>
+            <p>Fone de ouvido gamer sem fio premium com cancelamento de ruído e corpo leve.</p>
             <Link to="/" className={styles.shopButton}>
               Comprar Agora
             </Link>
@@ -31,7 +31,7 @@ const NewArrival = () => {
           <img src={Ultra} alt="Apple Watch Ultra" />
           <div className={styles.productInfo}>
             <h2>Apple Watch Ultra</h2>
-            <p>The Best in smartwatches, it can do everything.</p>
+            <p>Smartwatch de última geração. Perfeito para esportes e aventuras.</p>
             <Link to="/" className={styles.shopButton}>
               Comprar Agora
             </Link>
@@ -41,7 +41,7 @@ const NewArrival = () => {
           <img src={Zenbook} alt="Zenbook" />
           <div className={styles.productInfo}>
             <h2>Asus Zenbook S14</h2>
-            <p>Thin, fast, lightweight, the best of Asus.</p>
+            <p>CPU Intel Core de última geração. Design leve e fino.</p>
             <Link to="/" className={styles.shopButton}>
               Comprar Agora
             </Link>
@@ -51,7 +51,7 @@ const NewArrival = () => {
           <img src={Iphone15} alt="Iphone 15 Pro" />
           <div className={styles.productInfo}>
             <h2>Apple Iphone 15 Pro</h2>
-            <p>Iphone 15 Pro the best among others.</p>
+            <p>Experimente o iPhone de uma maneira totalmente nova.</p>
             <Link to="/" className={styles.shopButton}>
               Comprar Agora
             </Link>
