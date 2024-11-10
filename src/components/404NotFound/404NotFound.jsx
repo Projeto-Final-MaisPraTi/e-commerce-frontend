d57e92949bbc32d0f735e00c98cb1d54056b8459
Gabriel W <gabriel.beck03@gmail.com>
2024-11-10 15:41:29 -0300

    mais correções de formatação

--- src/components/404NotFound/404NotFound.jsx
+++ src/components/404NotFound/404NotFound.jsx
@@ -2,15 +2,12 @@ import "./404NotFound.css";
 import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
-
   return (
     <div className="page-not-found">
       <h1>404 Não Encontrado</h1>
       <p>A página que você visitou não existe. Você pode voltar para a página inicial.</p>
       <Link to="/">
-        <button className="back-home-button">
-          Voltar para a página inicial
-        </button>
+        <button className="back-home-button">Voltar para a página inicial</button>
       </Link>
     </div>
   );
