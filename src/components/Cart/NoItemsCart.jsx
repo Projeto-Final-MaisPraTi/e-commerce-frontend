9e358b634ae8600679cb9d07851fc7d72322ab1b
Gabriel W <gabriel.beck03@gmail.com>
2024-11-20 03:36:47 -0300

    Alterador texto do componente NoItemsCart

--- src/components/Cart/NoItemsCart.jsx
+++ src/components/Cart/NoItemsCart.jsx
@@ -2,27 +2,30 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const LinkHomePage = styled.span`
-color: dodgerblue;
-    &:hover{
-        cursor: pointer;
-    }
-`
+  color: dodgerblue;
+  &:hover {
+    cursor: pointer;
+  }
+`;
 const Div = styled.div`
-text-align: center;
-width: 80%;
-margin: 150px 10%;
-`
+  text-align: center;
+  width: 80%;
+  margin: 150px 10%;
+`;
 
 export function NoItensCart() {
-    const navigate= useNavigate();
-    const LinkClick= ()=>{
-        navigate("/")
-    }
+  const navigate = useNavigate();
+  const LinkClick = () => {
+    navigate("/");
+  };
 
-    return(
-        <Div>
-        <h1>Parece que você ainda não tem nenhum item no seu carrinho.</h1>
-        <h1>Clique <LinkHomePage onClick={()=>LinkClick()}>aqui</LinkHomePage>  para ver oque temos a oferecer.</h1>
-        </Div>
-    )
+  return (
+    <Div>
+      <h1>Parece que não há produtos no seu carrinho.</h1>
+      <h1>
+        Clique <LinkHomePage onClick={() => LinkClick()}>aqui</LinkHomePage> para ver oque temos a
+        oferecer.
+      </h1>
+    </Div>
+  );
 }
