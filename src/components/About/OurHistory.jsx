c6f013a6a1cd4c53a210bbfef39ef37defb44de8
Gabriel W <gabriel.beck03@gmail.com>
2024-11-23 22:11:12 -0300

    Pequena correção de texto no About

--- src/components/About/OurHistory.jsx
+++ src/components/About/OurHistory.jsx
@@ -7,12 +7,15 @@ const OurHistory = () => {
     <div className="container my-5">
       <div className="row align-items-center">
         <div className="col-md-6">
-          <h2 className="mb-5 custom-title">Nossa história</h2> {/* Aplicada a classe personalizada */}
+          <h2 className="mb-5 custom-title">Nossa história</h2>
           <p className="custom-text">
-          Lançado em 2020, o Exclusive é o principal mercado de compras online da América do Norte com presença ativa no Canadá. Apoiado por uma diversas marcas, possuindo grande variedade de produtos e atendendo mais de 3 milhões de clientes em toda a região.
+            Lançado em 2020, o Exclusive é o principal mercado de compras online da América do Norte
+            com presença ativa no Canadá. Apoiado por diversas marcas, possuindo grande variedade de
+            produtos e atendendo mais de 3 milhões de clientes em toda a região.
           </p>
           <p className="mt-3 custom-text">
-          Buscamos oferecer aos consumidores o melhor da tecnologia, diversificando o estoque tanto quanto possível.
+            Buscamos oferecer aos consumidores o melhor da tecnologia, diversificando o estoque
+            tanto quanto possível.
           </p>
         </div>
         <div className="col-md-6">
